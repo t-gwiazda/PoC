51cbb2fbe0e6f028fe56f91ddf5ab9d989733f34
Jenkins Automation Server <jenkins@ip-172-31-45-213.eu-central-1.compute.internal>
2019-12-04 20:03:36 +0000

    commit

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "instance_flavor" {
-  default     = "t2.micro"
-  description = "Size of instance"
-}
+#variable "instance_flavor" {
+#  default     = "t2.micro"
+#  description = "Size of instance"
+#}
 
 data "aws_ami" "latest-amazon-linux2" {
   most_recent = true
@@ -27,15 +27,33 @@ data "aws_ami" "latest-amazon-linux2" {
   }
 }
 
+#select aws_security_group for sec_group_for_http_and_ssh_icmp
+data "aws_security_group" "tg-sg" {
+  tags = {
+    Name = "tg-sg"
+  }
+}
+
 resource "aws_instance" "server1" {
   #ami = "ami-0c6b1d09930fac512"
-  ami = "${data.aws_ami.latest-amazon-linux2.id}"
-  #instance_type = "t2.micro"
-  instance_type = "${var.instance_flavor}"
+#  ami = "${data.aws_ami.latest-amazon-linux2.id}"
+  ami = data.aws_ami.latest-amazon-linux2.id
+  instance_type = "t2.micro"
+#  instance_type = "${var.instance_flavor}"
+  vpc_security_group_ids      = [data.aws_security_group.tg-sg.id]
 
   tags = {
     Name = "server1"
   }
+  key_name                    = "linux-tg-key"
+  user_data                   = <<-EOF
+                #! /bin/bash
+                yum update -y
+                yum install httpd -y
+                echo "Server is running" >>  /var/www/html/index.html
+                service httpd start
+                chkconfig httpd on
+  EOF
 }
 
 output "server1_public_hostname" {
